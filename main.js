b5f5149387f4c662f40df2d0e0d087e4eb31024a
Siddharth Preetham <siddharthpreetham09@gmail.com>
2021-01-15 20:52:54 -0500

    added image to text analysis in main.js

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-const tesseract = require("node-tesseract-ocr")
+const tesseract = require("node-tesseract-ocr");
 
-const config = {
-  lang: "eng",
-  oem: 1,
-  psm: 3,
+//Add event listener after the window has been loaded
+window.onload = loaded;
+
+//function to call when the window has been loaded
+function loaded(){
+  //add an event listener 
+  document.getElementById('Read').addEventListener('click',startReading);
 }
 
-tesseract.recognize("image.jfif", config)
-  .then(text => {
-    console.log("Result:", text)
-  })
-  .catch(error => {
-    console.log(error.message)
-  })
+
+
+function startReading(){
+  //Get frame from camera feed
+
+
+
+
+  
+  //Analyse frame using tesseract
+  const config = {
+    lang: "eng",
+    oem: 1,
+    psm: 3,
+  }
+  let textData = ""
+  tesseract.recognize("image.jfif", config)
+    .then(text => {
+      console.log("Result:", text)
+      textData = text
+    })
+    .catch(error => {
+      console.log(error.message)
+      textData = "Sorry I didn't catch that."
+    })
+
+  //Generate audio based on the text
+
+}
+
+
