970d24b26bad0da590467f5b01c46e14913ac4fd
Siddharth Preetham <siddharthpreetham09@gmail.com>
2021-01-17 02:12:57 -0500

    got the image reader to work

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // const tesseract = require("node-tesseract-ocr");
 
+
 //Add event listener after the window has been loaded
 window.onload = loaded;
 
@@ -15,19 +16,21 @@ function loaded(){
   live();
 
 }
-console.log('whatever');
-
+let w = 0;
+let h=0;
 function live() {
   const video = document.getElementById('livevid');
+  // const canvas = document.getElementById('canvas');
   window.navigator.mediaDevices.getUserMedia(constraint)
   .then(stream => {
     video.srcObject = stream;
     video.onloadedmetadata = (e) => {
         video.play();
-
+        console.log("vidoe's"+video.videoWidth+" "+video.videoHeight);
+        console.log(w+" "+h);
         w = video.videoWidth;
-        h = video.videoHeight
-
+        h = video.videoHeight;
+        console.log(w+" "+h);
         canvas.width = w;
         canvas.height = h;
         };
@@ -40,12 +43,16 @@ function live() {
 
 
 function snapshot(){
-  var canvas = document.createElement('canvas');
-  var context = canvas.getContext("2d");
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext("2d");
   const video = document.getElementById('livevid');
+  console.log("vidoe's"+video.videoWidth+" "+video.videoHeight);
+  console.log(w+" "+h);
   context.drawImage(video, 0, 0, w, h);
-  var dataURI = canvas.toDataURL('image/jpeg');
-  localStorage.setItem("image", dataURI);
+  const dataURI = canvas.toDataURL('image/jpeg');
+  // localStorage.setItem("image", dataURI);
+  // const blob = canvas.toBlob(); 
+  return dataURI;
 }
 
 const constraint = 
@@ -59,26 +66,37 @@ const constraint =
         }
     }
 
-function startReading(){
-////Get frame from camera feed
-  snapshot();
-  console.log('called');
+//function to return the image text
+async function getText(dataURI){
+  console.log(dataURI);
+  const response = await fetch('/get_text',{method:'POST', headers:{'ContentType':'application/json'}, body:dataURI});
+  if (response.ok){
+    const json = await response.text();
+    return json;
+  }
+}
+
+async function startReading(){
+  //Get frame from camera feed
+  const uri = snapshot();
+
   //Analyse frame using tesseract
   const config = {
     lang: "eng",
     oem: 1,
     psm: 3,
   }
-  let textData = ""
-  tesseract.recognize("image.jfif", config)
-    .then(text => {
-      console.log("Result:", text)
-      textData = text
-    })
-    .catch(error => {
-      console.log(error.message)
-      textData = "Sorry I didn't catch that."
-    })
+  const textData = await (async()=>getText(uri))();
+  console.log(textData);
+  // tesseract.recognize("image.jfif", config)
+  //   .then(text => {
+  //     console.log("Result:", text)
+  //     textData = text
+  //   })
+  //   .catch(error => {
+  //     console.log(error.message)
+  //     textData = "Sorry I didn't catch that."
+  //   })
 
   //Generate audio based on the text
   const player = new talkify.TtsPlayer()
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 var path = require('path');
 app.use(express.static(__dirname ));
-// const tesseract = require("tesseract-ocr");
+const tesseract = require("node-tesseract-ocr");
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.text();
+app.use(jsonParser);
+const fs = require('fs');
+
+//From github repo that converts data-uri to image
+const ImageDataURI = require('image-data-uri');
+
 
 const config = {
   lang: "eng",
@@ -10,19 +18,55 @@ const config = {
   psm: 3,
 };
 
-// tesseract.recognize("image.jpg", config).then(text => {
-//     console.log("Result:", text)
-//   })
-//   .catch(error => {
-//     console.log(error.message)
-//   });
+tesseract.recognize("image.png", config).then(text => {
+    console.log("Result:", text)
+  })
+  .catch(error => {
+    console.log(error.message)
+  });
 
 app.get('/', function(req, res){
     res.sendFile(path.resolve('../index.html'));
 })
 
+function dataURItoBlob(dataURI) {
+  var byteStr;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteStr = atob(dataURI.split(',')[1]);
+  else
+      byteStr = unescape(dataURI.split(',')[1]);
+
+  var mimeStr = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  var arr= new Uint8Array(byteStr.length);
+  for (var i = 0; i < byteStr.length; i++) {
+      arr[i] = byteStr.charCodeAt(i);
+  }
+
+  return new Blob([arr], {type:mimeStr});
+}
+
+//function to save datauri to filepath
+function convertAndSave(dataURI, filepath){
+
+}
+
 app.post('/get_text', function(req, res){
     console.log(req.body);
+    // convertAndSave(req.body, 'image.png');
+    ImageDataURI.outputFile(req.body, 'image.jpeg');
+    // fs.writeFileSync('tmp/myfile.png', new Buffer(req.body, 'base64'));
+    tesseract.recognize("image.jpeg", config).then(text => {
+      console.log("Result:", text)
+    })
+    .catch(error => {
+      console.log(error.message)
+    });
+    res.writeHead(200, {'ContentType':'text/plain'});
+    res.write('text_exmaple');
+    res.end();
 });
+
+
 
 app.listen(8080);
